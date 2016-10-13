421656a56f5805354198bd492600dd75edabe628
Elliot Koh <elliot_koh_1997@yahoo.com.sg>
2016-10-14 01:57:05 +0800

    add bar list view cont

---
+++ BarListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BarListViewController.swift
+//  AfterDark
+//
+//  Created by Swee Har Ng on 14/10/16.
+//  Copyright © 2016 kohbroco. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BarListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
