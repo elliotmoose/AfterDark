ffdb419dd2a458759a4c34b83b0b9c9953300eab
Elliot Koh <elliot_koh_1997@yahoo.com.sg>
2016-10-14 17:31:52 +0800

    minor tweak

--- AfterDark/Bar.swift
+++ AfterDark/Bar.swift
@@ -1,9 +1,8 @@
-import foundation
 class Bar{
     var name : String
     var description : String
     var loc_long : float
     var loc_lat : float
-    var rating: {avg,amb,price,food,svc}
+    var rating: (avg,amb,price,food,svc)
 
 }
