e99322722c02b1c2302d1528bc3d2b4d53ff3b63
Elliot Koh <elliot_koh_1997@yahoo.com.sg>
2016-10-16 03:17:07 +0800

    added xib for bar list table view cell

---
+++ BarListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BarListTableViewCell.swift
+//  AfterDark
+//
+//  Created by Swee Har Ng on 16/10/16.
+//  Copyright © 2016 kohbroco. All rights reserved.
+//
+
+import UIKit
+
+class BarListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
